31e5a59d90952fcdba74fa79bb6a77fc163a86cc
Arina <arina.milyutina.0005@gmail.com>
2022-11-21 20:20:59 +0300

    commit3

--- Lesson1.java
+++ Lesson1.java
@@ -2,6 +2,7 @@ public class Lesson1 {
     public static void main(String[]args){
         System.out.println("Hello.How are you?");
         System.out.println("Hi.I'm fine.And you?");
+        System.out.println("It's ok.");
 
     }
 }
